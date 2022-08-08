1af29c6581f794ef4578df2116bf4264ec9987d1
Yasir Khan <yasirkhanone22@gmail.com>
2022-08-08 23:59:42 +0500

    js modofied

--- Digital-Clock/js/main.js
+++ Digital-Clock/js/main.js
@@ -3,3 +3,12 @@ const minute = document.getElementById("minutes")
 const seconds = document.getElementById("seconds")
 const ap_pm = document.getElementById("am__pm")
 
+
+function updaterClock() {
+
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+
+
+}
