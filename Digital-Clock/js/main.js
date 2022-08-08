b3bb5c81e12db90785889e43b21f2d435fdf13c4
Yasir Khan <yasirkhanone22@gmail.com>
2022-08-09 00:46:57 +0500

    js approved

--- Digital-Clock/js/main.js
+++ Digital-Clock/js/main.js
@@ -1,14 +1,20 @@
-const hour = document.getElementById("hour")
-const minute = document.getElementById("minutes")
-const seconds = document.getElementById("seconds")
-const ap_pm = document.getElementById("am__pm")
+// const hour = document.getElementById("hour")
+// const minute = document.getElementById("minutes")
+// const seconds = document.getElementById("seconds")
+// const ap_pm = document.getElementById("am__pm")
 
-
-function updaterClock() {
-
+function formatAMPM(date) {
     let h = new Date().getHours();
     let m = new Date().getMinutes();
     let s = new Date().getSeconds();
-
-
+    console.log(h)
+    console.log(m)
+    console.log(s)
+    var ampm = h >= 12 ? 'pm' : 'am';
+    h = h % 12;
+    h = h ? h : 12; // the hour '0' should be '12'
+    m = m < 10 ? '0' + m : m;
+    var strTime = h + ':' + m + ' ' + ampm;
+    return strTime;
 }
+console.log(formatAMPM(new Date()));
