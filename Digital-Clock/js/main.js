af2c357517c9055f074d56abc98cce66833c9860
Yasir Khan <yasirkhanone22@gmail.com>
2022-08-08 23:58:05 +0500

    javascript files are modified

---
+++ Digital-Clock/js/main.js
@@ -0,0 +1,5 @@
+const hour = document.getElementById("hour")
+const minute = document.getElementById("minutes")
+const seconds = document.getElementById("seconds")
+const ap_pm = document.getElementById("am__pm")
+
