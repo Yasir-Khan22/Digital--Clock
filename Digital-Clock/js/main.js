b2fd1079ba20d4a82c2afe87e2f7e178ec47cd1e
Yasir Khan <yasirkhanone22@gmail.com>
2022-08-09 11:37:49 +0500

    js updated

--- Digital-Clock/js/main.js
+++ Digital-Clock/js/main.js
@@ -10,12 +10,18 @@ function formatAMPM(date) {
     console.log(h)
     console.log(m)
     console.log(s)
-    var ampm = h >= 12 ? 'pm' : 'am';
+
     h = h % 12;
     h = h ? h : 12; // the hour '0' should be '12'
-    m = m < 10 ? '0' + m : m;
-    s = s < 10 ? '0' + m : m;
-    var strTime = h + ':' + m + ' ' + ampm;
-    return strTime;
+    m = m < 10 ? '0' + { m } : m;
+    s = s < 10 ? '0' + { s } : m;
+
+    var ampm = h >= 12 ? 'pm' : 'am';
+
+    hour.innerText = h;
+    minute.innerText = m;
+    seconds.innerText = s;
+    ampm.innerText = ampm;
+
 }
-console.log(formatAMPM(new Date()));
+formatAMPM()
